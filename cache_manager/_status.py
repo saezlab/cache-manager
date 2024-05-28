3bad6cd8ae92462f428da0ebe0400ca054fd9db8
deeenes <turei.denes@gmail.com>
2024-05-28 16:27:49 +0200

    `status` enum for status codes

---
+++ cache_manager/_status.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import enum
+
+class status(enum.Enum):
+
+    UNINITIALIZED = 0
+    WRITE = 1
+    FAILED = 2
+    READY = 3
+
+    @classmethod
+    def from_str(cls, name: str) -> status:
+
+        return cls.__dict__[name.upper()]
