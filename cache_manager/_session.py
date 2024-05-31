835f2f9d4c3705d8d1200926f203ca0805d2f9b4
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-05-31 10:30:23 +0200

    Added log link to session module

--- cache_manager/_session.py
+++ cache_manager/_session.py
@@ -1,6 +1,8 @@
 import functools as _ft
 
-from pypath_common import log as _log, session as _session
+from pypath_common import log as _read_log
+from pypath_common import session as _session
 
 session = _ft.partial(_session, 'cache_manager')
-log = _ft.partial(_log, 'cache_manager')
+log = _ft.partial(_read_log, 'cache_manager')
+_log = _session._logger.msg
