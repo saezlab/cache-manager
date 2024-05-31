9bd3f1e64827b9a72f2fe7ac8fa0fe091b166c52
deeenes <turei.denes@gmail.com>
2024-05-31 10:21:49 +0200

    `session`: create session the correct way + shortcut to `log`

--- cache_manager/__init__.py
+++ cache_manager/__init__.py
@@ -6,4 +6,5 @@ from ._metadata import __version__, __author__, __license__
 from ._metadata import metadata as _metadata
 from . import _session
 
-session = _session._session()
+session = _session.session()
+log = _session.log
