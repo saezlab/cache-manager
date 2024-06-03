8d23627f132c7146f865926713ffb1922b7131e6
bbazsi <bbazsi41@gmail.com>
2024-06-03 10:02:18 +0100

    add(init): Add init.py to the _data folder

--- cache_manager/__init__.py
+++ cache_manager/__init__.py
@@ -6,4 +6,4 @@ from ._metadata import __version__, __author__, __license__
 from ._metadata import metadata as _metadata
 from ._session import log, _log, session
 from ._cache import Cache
-
+from . import _data as data
