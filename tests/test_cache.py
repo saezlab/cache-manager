d68399dffe954b182f5a27bd5c0df878731c6822
bbazsi <bbazsi41@gmail.com>
2024-06-07 14:58:13 +0100

    add(test.test_create): Add the test_create test function

---
+++ tests/test_cache.py
@@ -0,0 +1,13 @@
+import pytest
+from cache_manager import utils
+
+
+class TestCache:
+
+    def test_create(self, test_cache):
+        hashname = utils.hash("testdb")
+        test_cache.create("testdb")
+        test_cache._execute("SELECT * FROM main")
+        keys = {it[1] for it in test_cache.cur.fetchall()}
+
+        assert hashname in keys
