e14ef1479ef2d5d73e2898ee9d6be1b62d73d274
bbazsi <bbazsi41@gmail.com>
2024-06-11 14:50:53 +0100

    Set pragma busy_timeout

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -1,12 +1,15 @@
 from cache_manager import _lock
 
+import sqlite3
 
 def test_lock(test_cache):
 
     con0 = test_cache.con
+    con0.execute('PRAGMA busy_timeout = 30')
     con1 = sqlite3.Connection(test_cache.path)
 
     with _lock.Lock(con1):
 
         test_cache.create('testlock')
 
+    con0.execute('PRAGMA busy_timeout = 30000')
