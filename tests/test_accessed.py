ebb22c2048bfa7c485928c76b0780a89dc27f4fe
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-06-21 15:33:14 +0200

    Finished `test_last_search`

--- tests/test_accessed.py
+++ tests/test_accessed.py
@@ -6,13 +6,15 @@ import datetime
 def test_last_search(test_cache):
 
     it = test_cache.create('testlastsearch')
-    t0 = datetime.datetime.now()
+    t0 = datetime.datetime.now().replace(microsecond=0)# - datetime.timedelta(1)
 
     test_cache.search('testlastsearch')
 
-    q = f'SELECT last_search, search_count FROM main WHERE id = {it._id};'
-    last = test_cache._execute(q).fetchone()[0]
-    t1 = datetime.datetime.strptime(last[0], '%Y-%m-%d %H:%M:%S')
+    q = f'SELECT datetime(last_search, "localtime"), search_count FROM main WHERE id = {it._id};'
+    test_cache._execute(q)
+    last, count = test_cache.cur.fetchone()
 
-    assert t1 > t0
-    assert int(last[1]) == 1
+    t1 = datetime.datetime.strptime(last, '%Y-%m-%d %H:%M:%S')
+
+    assert t1 >= t0
+    assert int(count) == 1
