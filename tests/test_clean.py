a3b92c671ad925c8ba80d7614c1d576c7bbafd06
deeenes <turei.denes@gmail.com>
2024-08-02 16:08:08 +0200

    test for `autoclean`

--- tests/test_clean.py
+++ tests/test_clean.py
@@ -1,6 +1,8 @@
 import pytest
 import os
 
+from cache_manager import _status
+
 
 def test_clean_disk(test_cache):
 
@@ -30,3 +32,21 @@ def test_clean_db(test_cache):
     search = test_cache.search('test-clean-db', include_removed = True)
 
     assert len(search) == 0
+
+
+def test_autoclean(test_cache):
+
+    items = [
+        test_cache.create('test-autoclean')
+        for _ in range(3)
+    ]
+
+    items[1].status = _status.status.READY.value
+
+    test_cache.autoclean()
+
+    items = test_cache.search('test-autoclean', include_removed = True)
+
+    assert len(items) == 1
+    assert items[0].version == 2
+    assert items[0].status == _status.status.READY.value
