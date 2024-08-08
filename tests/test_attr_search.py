8eed33828f77b0c659d008bc6da1c76edef69108
deeenes <turei.denes@gmail.com>
2024-08-08 14:23:03 +0200

    search by attrs: test search in more complex situation

--- tests/test_attr_search.py
+++ tests/test_attr_search.py
@@ -6,3 +6,21 @@ def test_search_by_attrs(test_cache):
     result = test_cache.search(attrs = {'foo': 'bar'})
 
     assert result[0].key == it.key
+
+
+def test_search_by_attrs2(test_cache):
+
+    it0 = test_cache.best_or_new('wrong-one-1', attrs = {'foo': 'baz'})
+    it1 = test_cache.best_or_new('wrong-one-2', attrs = {'foo': 'bar'})
+    it2 = test_cache.best_or_new('good-one', attrs = {'foo': 'bar'})
+
+    result = test_cache.search('good-one', attrs = {'foo': 'bar'})
+
+    assert len(result) == 1
+    assert result[0].key == it2.key
+    assert result[0].attrs['foo'] == 'bar'
+    assert result[0].uri == 'good-one'
+
+    result = test_cache.search('good-one', attrs = {'foo': 'baz'})
+
+    assert len(result) == 0
