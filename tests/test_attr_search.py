8fce5450f66aad163c26471b554a40a836df9766
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-08-08 14:43:31 +0200

    Added tests for `test_attr_search` for int and datetime

--- tests/test_attr_search.py
+++ tests/test_attr_search.py
@@ -1,5 +1,14 @@
+from dateutil.parser import parse as dateparse
 
-def test_search_by_attrs(test_cache):
+__all__ = [
+    'test_search_by_attrs_datetime',
+    'test_search_by_attrs_extended',
+    'test_search_by_attrs_int',
+    'test_search_by_attrs_simple',
+]
+
+
+def test_search_by_attrs_simple(test_cache):
 
     it = test_cache.best_or_new('search_by_attrs', attrs = {'foo': 'something'})
 
@@ -8,7 +17,7 @@ def test_search_by_attrs(test_cache):
     assert result[0].key == it.key
 
 
-def test_search_by_attrs2(test_cache):
+def test_search_by_attrs_extended(test_cache):
 
     it0 = test_cache.best_or_new('wrong-one-1', attrs = {'foo': 'baz'})
     it1 = test_cache.best_or_new('wrong-one-2', attrs = {'foo': 'bar'})
@@ -29,3 +38,36 @@ def test_search_by_attrs2(test_cache):
 
     assert len(result) == 2
     assert {r.uri for r in result} == {'good-one', 'wrong-one-2'}
+
+def test_search_by_attrs_int(test_cache):
+    it = test_cache.best_or_new('search_by_attrs_int', attrs = {'foo': 123})
+
+    result = test_cache.search(attrs = {'foo': 123})
+
+    assert result[0].key == it.key
+
+    result = test_cache.search(attrs = {'foo<=': 500})
+
+    assert result[0].key == it.key
+
+    result = test_cache.search(attrs = {'foo>=': 500})
+
+    assert len(result) == 0
+
+def test_search_by_attrs_datetime(test_cache):
+    it = test_cache.best_or_new(
+        'search_by_attrs_datetime',
+        attrs = {'foo': dateparse('2020/12/31')},
+    )
+
+    result = test_cache.search(attrs = {'foo': dateparse('2020/12/31')})
+
+    assert result[0].key == it.key
+
+    result = test_cache.search(attrs = {'foo<=': dateparse('2021/12/31')})
+
+    assert result[0].key == it.key
+
+    result = test_cache.search(attrs = {'foo>=': dateparse('2021/12/31')})
+
+    assert len(result) == 0
