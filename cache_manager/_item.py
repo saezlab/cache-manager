e4ce2635602b133a1eb2eb61e645aadd20c841b1
Nicolas Palacio <nicolaspalacio91@gmail.com>
2024-08-19 16:35:39 +0200

    [Doc] Added docstrings to `CacheItem` and its methods

--- cache_manager/_item.py
+++ cache_manager/_item.py
@@ -19,8 +19,91 @@ class CacheItem:
     """
     Cache item class, stores a single cache item information.
 
-    NOTE: Actual creation function used is the class method `new` and not
-    `__init__`.
+    NOTE: Alternative creation function can be found in the class method `new`.
+    The `__init__` method creates an instance based on the `key` attribute
+    (generally for internal use), while the `new` method takes a URI and is more
+    intended for user interface.
+
+    Args:
+        key:
+            Unique key name for the item. The creation method
+            `CacheItem.new` provides it automatically as an alphanumeric
+            string (see `CacheItem.serialize` for details).
+        version:
+            Version number of the item. Optional, defaults to `1`.
+        status:
+            Status of the entry as integer (see `_status.Status` for more
+            info). Optional, defaults to `1`.
+        date:
+            Date of the entry, if none is provided, takes the current time.
+            Optional, defaults to `None`.
+        filename:
+            Name of the file associated to the item. Optional, defaults to
+            `None`.
+        ext:
+            Extension of the file associated to the item. Optional, defaults
+            to `None` (automatically extracted from the file name).
+        label:
+            Label for the item (e.g. type, group, category...). Optional,
+            defaults to `None`
+        attrs:
+            Extra attributes associated to the item. Keys are the attribute
+            names and values their content. These attributes will be stored
+            in the attribute tables according to their data type
+            automatically. Optional, defaults to `None`.
+        _id:
+            Internal ID number. Optional, defaults to `None`.
+        last_read:
+            Date of last reading of the entry. Optional, defaults to `None`.
+        last_search:
+            Date of last search for the item. Optional, defaults to `None`.
+        read_count:
+            Counter of reads for the item. Optional, defaults to `None`.
+        search_count:
+            Counter for the times the item has been searched. Optional,
+            defaults to `None`.
+        cache:
+            The `Cache` instance where the item belongs. Optional, defaults
+            to `None`.
+
+    Attrs:
+        key:
+            Unique key name for the item.
+        version:
+            Current version number of the item.
+        date:
+            Creation date of the entry.
+        filename:
+            Name of the file associated to the item.
+        ext:
+            Extension of the file associated to the item.
+        label:
+            Item label (e.g. type, group, category...).
+        attrs:
+            Extra attributes associated to the item. Keys are the attribute
+            names and values their content.
+        last_read:
+            Date of last reading of the entry.
+        last_search:
+            Date of last search for the item.
+        read_count:
+            Counter of reads for the item.
+        search_count:
+            Counter for the times the item has been searched.
+        cache:
+            The `Cache` instance where the item belongs.
+        cache_fname:
+            The file name of the associated cache database.
+        path:
+            Path to the file associated to the item.
+        rstatus:
+            Current status of the item instance.
+        status:
+            Status of the item as registered in the `Cache`.
+        uri:
+            Uniform Resource Identifier.
+        version_id:
+            Identifier as a combination of item key and version number.
     """
 
     def __init__(
@@ -40,49 +123,6 @@ class CacheItem:
             search_count: int | None = None,
             cache: Type[cache_manager.Cache] | None = None,
     ):
-        """
-        Args:
-            key:
-                Unique key name for the item. The creation method
-                `CacheItem.new` provides it automatically as an alphanumeric
-                string (see `CacheItem.serialize` for details).
-            version:
-                Version number of the item. Optional, defaults to `1`.
-            status:
-                Status of the entry as integer (see `_status.Status` for more
-                info). Optional, defaults to `1`.
-            date:
-                Date of the entry, if none is provided, takes the current time.
-                Optional, defaults to `None`.
-            filename:
-                Name of the file associated to the item. Optional, defaults to
-                `None`.
-            ext:
-                Extension of the file associated to the item. Optional, defaults
-                to `None`.
-            label:
-                Label for the item (e.g. type, group, category...). Optional,
-                defaults to `None`
-            attrs:
-                Extra attributes associated to the item. Keys are the attribute
-                names and values their content. These attributes will be stored
-                in the attribute tables according to their data type
-                automatically. Optional, defaults to `None`.
-            _id:
-                Internal ID number. Optional, defaults to `None`.
-            last_read:
-                Date of last reading of the entry. Optional, defaults to `None`.
-            last_search:
-                Date of last search for the item. Optional, defaults to `None`.
-            read_count:
-                Counter of reads for the item. Optional, defaults to `None`.
-            search_count:
-                Counter for the times the item has been searched. Optional,
-                defaults to `None`.
-            cache:
-                The `Cache` instance where the item belongs. Optional, defaults
-                to `None`.
-        """
 
         self.key = key
         self.version = version
@@ -109,7 +149,6 @@ class CacheItem:
         )
 
 
-    # FIXME: Shouldn't we override the __new__ method in this case?
     @classmethod
     def new(
         cls,
@@ -127,9 +166,53 @@ class CacheItem:
         read_count: int = 0,
         search_count: int = 0,
         cache: None = None,
-    ):
+    ) -> CacheItem:
         """
         Creates a new item.
+
+        Args:
+            uri:
+                Uniform Resource Identifier. Optional, defaults to `None`.
+            params:
+                Collection of parameters passed to the `__init__` method in dict
+                 format where key-value pairs correspond to parameter-value
+                 respectively. Optional, defaults to `None`.
+            version:
+                The version nuber of the new item. Optional, defaults to `0`.
+            status:
+                Status of the new item. Optional, defaults to `0`.
+            date:
+                Date of the entry, if none is provided, takes the current time.
+                Optional, defaults to `None`.
+            filename:
+                Name of the file associated to the item. Optional, defaults to
+                `None`.
+            ext:
+                Extension of the file associated to the item. Optional, defaults
+                to `None` (automatically extracted from the file name).
+            label:
+                Label for the item (e.g. type, group, category...). Optional,
+                defaults to `None`
+            attrs:
+                Extra attributes associated to the item. Keys are the attribute
+                names and values their content. These attributes will be stored
+                in the attribute tables according to their data type
+                automatically. Optional, defaults to `None`.
+            last_read:
+                Date of last reading of the entry. Optional, defaults to `None`.
+            last_search:
+                Date of last search for the item. Optional, defaults to `None`.
+            read_count:
+                Counter of reads for the item. Optional, defaults to `0`.
+            search_count:
+                Counter for the times the item has been searched. Optional,
+                defaults to `0`.
+            cache:
+                The `Cache` instance where the item belongs. Optional, defaults
+                to `None`.
+
+        Returns:
+            Instance of the newly created `CacheItem`.
         """
 
         params = params or {}
@@ -149,9 +232,17 @@ class CacheItem:
 
 
     @classmethod
-    def serialize(cls, params: dict | None = None):
+    def serialize(cls, params: dict | None = None) -> str:
         """
-        Serializes to generate an identifier.
+        Serializes the parameters dictionary and hashes the result to generate
+        an identifier. See `_utils.hash` for more information.
+
+        Args:
+            params:
+                The parameters dictionary too generate the hash from.
+
+        Returns:
+            The resulting hash from the serailized parameters.
         """
 
         params = params or {}
@@ -160,7 +251,13 @@ class CacheItem:
 
 
     @property
-    def cache_fname(self):
+    def cache_fname(self) -> str:
+        """
+        Gives the file name as stored in the cache.
+
+        Returns:
+            Combination of `version_id` (key + version) and the file extension.
+        """
 
         ext = f'.{self.ext}' or ''
 
@@ -168,9 +265,13 @@ class CacheItem:
 
 
     @property
-    def path(self):
+    def path(self) -> str:
         """
-        Defines the path of the file.
+        Gives the path of the file.
+
+        Returns:
+            The path to the cache item file (generally the cache path + the
+            `cache_fname`).
         """
 
         d = self.cache.dir if self.cache else ''
@@ -179,39 +280,73 @@ class CacheItem:
 
 
     @property
-    def rstatus(self):
+    def rstatus(self) -> int:
+        """
+        Gives the current status for the cache item instance.
+
+        Returns:
+            The integer value defining the status.
+        """
 
         return self._status
 
 
     @property
-    def status(self):
+    def status(self) -> int:
+        """
+        Gives the current status for the cache item as stored in the cache (if
+        any, otherwise returns the status as stored in the `CacheItem`).
+
+        Returns:
+            The integer value defining the status.
+        """
 
         return getattr(self._from_main(), '_status', self._status)
 
 
     @status.setter
     def status(self, value: int):
+        """
+        Sets the status to a given value, updating it in the associated `Cache`
+        instance if available.
+
+        Args:
+            value:
+                The value defining the new status to be set.
+        """
 
         if self.cache:
 
             self.cache.update_status(
-                key = self.key,
-                version = self.version,
-                status = value,
+                key=self.key,
+                version=self.version,
+                status=value,
             )
 
         self._status = value
 
 
     @property
-    def uri(self):
+    def uri(self) -> str | None:
+        """
+        Provides the Uniform Resource Identifier if available.
+
+        Returns:
+            The Uniform Resource Identifier or `None` if not defined.
+        """
 
         return self.attrs.get('_uri', None)
 
 
     @property
-    def version_id(self):
+    def version_id(self) -> str:
+        """
+        Gives the unique version identifier based on the key of the item and the
+        version number.
+
+        Returns:
+            Version identifier as [key]-[version].
+        """
 
         return f'{self.key}-{self.version}'
 
@@ -226,7 +361,16 @@ class CacheItem:
 
     def open(self, **kwargs) -> str | IO | dict[str, str | IO] | None:
         """
-        Opens the file in reading mode
+        Opens the file in reading mode.
+
+        Args:
+            **kwargs:
+                Keyword arguments are passed directly to the `Opener` class.
+
+        Returns:
+            The resulting opened file content. The type of content will depend
+            on the passed arguments. See the `Opener` documentation for more
+            details.
         """
 
         if self.status == Status.READY.value:
@@ -244,20 +388,36 @@ class CacheItem:
 
     def remove(self, disk: bool = False, keep_record: bool = True):
         """
-        Remove the item from the database.
+        Removes the item from the cache database (if available).
+
+        Args:
+            disk:
+                Whether to also remove the file from disk. Optional, defaults to
+                `False`.
+            keep_record:
+                Whether to keep the record in the database (i.e. marks the item
+                status as trashed without actually deleting the record).
+                Optional, defaults to `True`.
         """
 
         if self.cache:
 
             self.cache.remove(
-                key = self.key,
-                version = self.version,
-                disk = disk,
-                keep_record = keep_record,
+                key=self.key,
+                version=self.version,
+                disk=disk,
+                keep_record=keep_record,
             )
 
 
     def _from_main(self) -> CacheItem | None:
+        """
+        Retrieves the cache item instance from the cache database instance if
+        available.
+
+        Returns:
+            The `CacheItem` instance retrieved as in the database.
+        """
 
         if self.cache:
 
@@ -265,6 +425,19 @@ class CacheItem:
 
 
     def _open(self, **kwargs) -> _open.Opener:
+        """
+        Generates the `Opener` instance that opens the item file.
+
+        Args:
+            **kwargs:
+                Keyword arguments are passed directly to the `Opener` class.
+
+        Returns:
+            The resulting `Opener` instance with the opened file. The type of
+            content will depend on the passed arguments. See the `Opener`
+            documentation for more details.
+        """
+
         self.cache._accessed(self._id)
 
         return _open.Opener(self.path, **kwargs)
@@ -272,7 +445,8 @@ class CacheItem:
 
     def _setup(self):
         """
-        Setting default values
+        Sets up default values for a new instance of a cache item. This infers
+        file name, extension and date in case these are not provided.
         """
 
         self.filename = (
